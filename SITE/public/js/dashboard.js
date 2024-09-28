243aab9a4ca522813b805c4867e836aad52b7403
MathSoares88 <matheus.ssoares@sptech.school>
2024-09-27 22:45:14 -0300

    Tela da IA e Login e Cadastro atualizadas

---
+++ SITE/public/js/dashboard.js
@@ -0,0 +1,43 @@
+
+    document.addEventListener('DOMContentLoaded', function () {
+        const toggleButton = document.getElementById('theme-toggle');
+        const themeIcon = document.getElementById('theme-icon');
+        const dashButton = document.getElementById('theme-dash');
+        const themelogo = document.getElementById('theme-logo');
+        const themenoti = document.getElementById('theme-noti');
+        const themeconfg = document.getElementById('theme-confg');
+
+        const body = document.body;
+
+        // Verifica o tema atual ao carregar a página
+        if (localStorage.getItem('theme') === 'dark') {
+            body.classList.add('dark-mode');
+            themeIcon.src = './assets/icon/Moon.png';
+            themelogo.src = './assets/img/LOGO-Dark.png';
+            themenoti.src = './assets/icon/icons8-sino-50.png';
+            themeconfg.src = './assets/icon/icons8-configuração-50.png';
+        }
+
+        function toggleTheme() {
+            body.classList.toggle('dark-mode');
+
+            if (body.classList.contains('dark-mode')) {
+                themeIcon.src = './assets/icon/Moon.png';
+                themelogo.src = './assets/img/LOGO-Dark.png';
+                themenoti.src = './assets/icon/icons8-sino-50.png';
+                themeconfg.src = './assets/icon/icons8-configuração-50.png';
+                localStorage.setItem('theme', 'dark');
+            } else {
+                themeIcon.src = './assets/icon/Light.png';
+                themelogo.src = './assets/img/LOGO.png';
+                themenoti.src = './assets/icon/image 35.png';
+                themeconfg.src = './assets/icon/image 34.png';
+                localStorage.setItem('theme', 'light');
+            }
+        }
+
+
+        toggleButton.addEventListener('click', toggleTheme);
+
+        dashButton.addEventListener('click', toggleTheme);
+    });
