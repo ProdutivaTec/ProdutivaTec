805a83fa333a583f2f37dcd68ab47a218c2c067e
Victor Moralles Araujo Paiva <160356771+Vmoralles@users.noreply.github.com>
2024-10-31 17:42:23 -0300

    Atualizando app.js

--- SITE/app.js
+++ SITE/app.js
@@ -1,5 +1,4 @@
-// Define o ambiente: 'producao' ou 'desenvolvimento'
-var ambiente_processo = process.env.AMBIENTE_PROCESSO || 'desenvolvimento';
+var ambiente_processo = 'producao'
 
 var caminho_env = ambiente_processo === 'producao' ? '.env' : '.env.dev';
 require("dotenv").config({ path: caminho_env });
@@ -8,13 +7,12 @@ var express = require("express");
 var cors = require("cors");
 var path = require("path");
 
-// Obtém as variáveis de ambiente do arquivo .env
-var PORTA_APP = process.env.APP_PORT || 3000;  // Usa 3000 se não definido
-var HOST_APP = process.env.APP_HOST || 'localhost';  // Usa 'localhost' se não definido
+var PORTA_APP = process.env.APP_PORT 
+var HOST_APP = process.env.APP_HOST 
 
 var app = express();
 
-// Configura rotas
+
 var indexRouter = require("./src/routes/index");
 var usuarioRouter = require("./src/routes/usuarios");
 
@@ -23,20 +21,17 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
-// Middleware de CORS
+
 app.use(cors());
 
-// Definir as rotas da aplicação
 app.use("/", indexRouter);
 app.use("/usuarios", usuarioRouter);
 
-// Verifica se as variáveis de ambiente foram definidas corretamente
 if (!PORTA_APP || !HOST_APP) {
     console.error("ERRO: As variáveis de ambiente APP_HOST e APP_PORT não estão definidas.");
     process.exit(1);  // Interrompe o processo em caso de erro
 }
 
-// Inicia o servidor
 app.listen(PORTA_APP, function () {
     console.log(`
     .: http://${HOST_APP}:${PORTA_APP} :. \n\n`);
